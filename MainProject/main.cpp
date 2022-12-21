d6b7118e3462c04183fe00c9412bc912589aed5b
Дарья Провальская <kaamomee@gmail.com>
2022-12-21 20:11:06 +0300

    Добавлены тестовые данные

--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -5,3 +5,15 @@ int main()
     std::cout << "12 Veb 10 Variant Provalskaya Darya\n";
     return 0;
 }
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #9. GIT\n";
+    cout << "Variant #10. Product catalog\n";
+    cout << "Author: Provalskaya Darya\n";
+    cout << "Group: 12\n";
+    return 0;
+}
